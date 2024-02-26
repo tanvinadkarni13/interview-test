ee823e9799fcb314456fe56127b248f37addc9bd
tanvinadkarni13 <159175942+tanvinadkarni13@users.noreply.github.com>
2024-02-25 20:15:00 -0800

    Added request functionality.
    1. Key assumptions
    * The README file instructions about buttons are different from the final results images. The button should have blue class when it is wrapped in <b> and not vice versa. 
    * I have not edited/removed any existing CSS and kept my CSS changes strictly additive.
    
    2. Animations 
    Added only basic animations, have not spent too much time making them best looking.

--- index.js
+++ index.js
@@ -13,63 +13,110 @@ const FAQ = [
     }
 ];
 
-// add code here if needed
 
+/**
+ * For any given element it extracts the color from the first paragraph and sets it as the background color of the element and then deleted this paragraph.
+ * This method should not be called on an element where the first paragraph does not contain a color.
+ */
 function processBackgroundColor(el) {
-    // add code here
+    el.querySelectorAll('p:first-child').forEach(p => {
+        // if the p contains color. 
+        if(p.textContent.indexOf('#')==0){
+            el.style.backgroundColor = p.textContent;
+        }else{
+            el.style.backgroundImage = p.textContent;
+        }
+    })
+    el.querySelector('div:first-child').remove();
 }
+
 function processHero(el) {
     processBackgroundColor(el);
-    // add code here
+    const paragraphsWithLinks = document.querySelectorAll('p a');
+    paragraphsWithLinks.forEach(link => {
+      link.closest('p').classList.add('action-area');
+    });
+
+    el.querySelectorAll('p.action-area').forEach(p => {
+        processButtons(p, 'i',['con-button'])
+        processButtons(p, 'b',['con-button', 'blue'])
+    });
 }
+
+// This function remove the wrapping tags and applies classes to the buttons.
+function processButtons(el, wrapperTag, buttonClasses){
+    const wrapperElements = el.querySelectorAll(wrapperTag);
+    wrapperElements.forEach(wrapperElement => {
+        while (wrapperElement.firstChild) {
+            wrapperElement.firstChild.classList.add(...buttonClasses);
+            wrapperElement.parentNode.insertBefore(wrapperElement.firstChild, wrapperElement); 
+        }
+        wrapperElement.remove();
+    });
+}
+
 function processBrick(el) {
     processBackgroundColor(el);
-    // add code here
+    const paragrpahs = el.querySelectorAll('p');
+    if(paragrpahs.length == 3){
+        processBrickContent(paragrpahs);
+    }
+    
 }
+
+function processBrickContent(p){
+    p[0].classList.add('title');
+    p[1].classList.add('price');
+    p[2].classList.add('description');
+}
+
 function processFaq(el) {
-    // improve this code
-    el.innerHTML = `
-        <div class="faq-set">
-            <div class="question">
-                <div>
-                    <h3>${FAQ[0].q}</h3>
+    let faqHTML = '';
+    for (let i = 0; i < FAQ.length; i++) {
+        faqHTML += `
+            <div class="faq-set">
+                <div id="question-${i}" class="question " onClick="toggleAnswer(${i})">
+                    <div>
+                        <h3>${FAQ[i].q}</h3>
+                    </div>
                 </div>
-            </div>
-            <div class="answer">
-                <div>
-                    <p>${FAQ[0].a}</p>
+                <div id="answer-${i}" class="answer  hidden">
+                    <div>
+                        <p>${FAQ[i].a}</p>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="faq-set">
-            <div class="question">
-                <div>
-                    <h3>${FAQ[1].q}</h3>
-                </div>
-            </div>
-            <div class="answer">
-                <div>
-                    <p>${FAQ[1].a}</p>
-                </div>
-            </div>
-        </div>
-        <div class="faq-set">
-            <div class="question">
-                <div>
-                    <h3>${FAQ[2].q}</h3>
-                </div>
-            </div>
-            <div class="answer">
-                <div>
-                    <p>${FAQ[2].a}</p>
-                </div>
-            </div>
-        </div>`;
-    // add code here
+            </div>`;
+    }
+    el.innerHTML = faqHTML;
 }
+
+// This function toggles the answer and question classes.
+// This is a global function and assumed only one set of FAQs are present on the page.
+function toggleAnswer(i) {
+    document.getElementById(`answer-${i}`).classList.toggle('hidden');
+    document.getElementById(`question-${i}`).classList.toggle('expanded');
+}
+
 function processBanner(el) {
-    // add code here
+    let heroElement = document.querySelector('.hero');
+    // Detect when .hero is visible
+    let observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                el.classList.remove('banner-visible')
+            } else {
+                el.classList.add('banner-visible')
+            }
+        });
+    });
+    observer.observe(heroElement);
+
+    processButtons(el, 'b',['con-button', 'blue']);
+    el.classList.add('bottom-fixed');
+    let bannerHeight = el.offsetHeight;
+    document.body.style.paddingBottom = bannerHeight + 'px';
 }
+
 document.querySelectorAll('.hero').forEach(processHero);
 document.querySelectorAll('.brick').forEach(processBrick);
 document.querySelectorAll('.faq').forEach(processFaq);
